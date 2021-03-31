84d44a0a75fb3951d6ab379634ba245eb1833bae
Narahari7RK <66745531+Narahari7RK@users.noreply.github.com>
2021-03-31 16:50:34 -0400

    Update Testcase1.java

--- src/test/java/com/qa/testcases/Testcase1.java
+++ src/test/java/com/qa/testcases/Testcase1.java
@@ -17,6 +17,8 @@ public class Testcase1 {
 	@Test(priority=1)
 	public void searchPage() throws InterruptedException {
 		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\narah\\eclipse-workspace\\Final\\chromedriver.exe");
+		
 		// 1. Open chrome browser
 		
 		driver = new ChromeDriver();
